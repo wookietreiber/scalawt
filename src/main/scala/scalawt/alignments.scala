33b207bda085c6e7c9433b97b6471669905ade57
Christian Krause <kizkizzbangbang@googlemail.com>
2011-07-19 14:03:33 +0200

    alignments

---
+++ src/main/scala/scalawt/alignments.scala
@@ -0,0 +1,139 @@
+/* **************************************************************************
+ *                                                                          *
+ *  Copyright (C)  2011  Christian Krause                                   *
+ *                                                                          *
+ *  Christian Krause <wookietreiber@users.berlios.de>                       *
+ *                                                                          *
+ ****************************************************************************
+ *                                                                          *
+ *  This file is part of 'scalawt'.                                         *
+ *                                                                          *
+ *  This project is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  any later version.                                                      *
+ *                                                                          *
+ *  This project is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this project. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+
+package scalawt
+
+/** Holds the actual alignment objects. */
+object Alignment {
+
+	// -----------------------------------------------------------------------
+	// direction alignments
+	// -----------------------------------------------------------------------
+
+	case object Top extends Direction with Vertical {
+		/** Returns [[scalawt.Alignment.Bottom]]. */
+		override val opposite = Bottom
+	}
+
+	case object Bottom extends Direction with Vertical {
+		/** Returns [[scalawt.Alignment.Top]]. */
+		override val opposite = Top
+	}
+
+	case object Left extends Direction with Horizontal with Text {
+		/** Returns [[scalawt.Alignment.Right]]. */
+		override val opposite = Right
+	}
+
+	case object Right extends Direction with Horizontal with Text {
+		/** Returns [[scalawt.Alignment.Left]]. */
+		override val opposite = Left
+	}
+
+	case object TopLeft extends Direction {
+		/** Returns [[scalawt.Alignment.BottomRight]]. */
+		override val opposite = BottomRight
+	}
+
+	case object BottomRight extends Direction {
+		/** Returns [[scalawt.Alignment.TopLeft]]. */
+		override val opposite = TopLeft
+	}
+
+	case object TopRight extends Direction {
+		/** Returns [[scalawt.Alignment.BottomLeft]]. */
+		override val opposite = BottomLeft
+	}
+
+	case object BottomLeft extends Direction {
+		/** Returns [[scalawt.Alignment.TopRight]]. */
+		override val opposite = TopRight
+	}
+
+	// -----------------------------------------------------------------------
+	// text alignments
+	// -----------------------------------------------------------------------
+
+	case object Center extends Horizontal with Vertical with Text
+
+	case object Leading extends Text
+
+	case object Trailing extends Text
+
+	case object Justified extends Text
+}
+
+/** Base of the alignment hierarchy.
+	*
+	* @see [[scalawt.Direction]]
+	* @see [[scalawt.Horizontal]]
+	* @see [[scalawt.Vertical]]
+	* @see [[scalawt.Text]]
+	*/
+sealed trait Alignment
+
+/** Base of alignments used for direction-based layouts and movement.
+	*
+	* @see [[scalawt.Alignment.Top]]
+	* @see [[scalawt.Alignment.Bottom]]
+	* @see [[scalawt.Alignment.Left]]
+	* @see [[scalawt.Alignment.Right]]
+	* @see [[scalawt.Alignment.TopLeft]]
+	* @see [[scalawt.Alignment.BottomRight]]
+	* @see [[scalawt.Alignment.TopRight]]
+	* @see [[scalawt.Alignment.BottomLeft]]
+	*/
+sealed trait Direction extends Alignment {
+	/** Returns the opposite direction. */
+	def opposite: Alignment
+}
+
+/** Base of the horizontal alignment hierarchy.
+	*
+	* @see [[scalawt.Alignment.Left]]
+	* @see [[scalawt.Alignment.Right]]
+	* @see [[scalawt.Alignment.Center]]
+	*/
+sealed trait Horizontal extends Alignment
+
+/** Base of the vertical alignment hierarchy.
+	*
+	* @see [[scalawt.Alignment.Top]]
+	* @see [[scalawt.Alignment.Bottom]]
+	* @see [[scalawt.Alignment.Center]]
+	*/
+sealed trait Vertical extends Alignment
+
+/** Base of alignments that can be used to align text.
+	*
+	* @see [[scalawt.Alignment.Left]]
+	* @see [[scalawt.Alignment.Right]]
+	* @see [[scalawt.Alignment.Center]]
+	* @see [[scalawt.Alignment.Justified]]
+	* @see [[scalawt.Alignment.Leading]]
+	* @see [[scalawt.Alignment.Trailing]]
+	*/
+sealed trait Text extends Alignment
---
+++ src/test/scala/scalawt/AlignmentSpec.scala
@@ -0,0 +1,84 @@
+/* **************************************************************************
+ *                                                                          *
+ *  Copyright (C)  2011  Christian Krause                                   *
+ *                                                                          *
+ *  Christian Krause <wookietreiber@users.berlios.de>                       *
+ *                                                                          *
+ ****************************************************************************
+ *                                                                          *
+ *  This file is part of 'scalawt'.                                         *
+ *                                                                          *
+ *  This project is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  any later version.                                                      *
+ *                                                                          *
+ *  This project is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this project. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+
+package scalawt
+
+import Alignment._
+import org.specs2.mutable._
+
+class AlignmentSpec extends Specification {
+
+	// -----------------------------------------------------------------------
+	// category specifications
+	// -----------------------------------------------------------------------
+
+	"Direction" should {
+		"include Top"         in { Top         must beLike { case x: Direction => ok } }
+		"include Bottom"      in { Bottom      must beLike { case x: Direction => ok } }
+		"include Left"        in { Left        must beLike { case x: Direction => ok } }
+		"include Right"       in { Right       must beLike { case x: Direction => ok } }
+		"include TopLeft"     in { TopLeft     must beLike { case x: Direction => ok } }
+		"include TopRight"    in { TopRight    must beLike { case x: Direction => ok } }
+		"include BottomLeft"  in { BottomLeft  must beLike { case x: Direction => ok } }
+		"include BottomRight" in { BottomRight must beLike { case x: Direction => ok } }
+	}
+
+	"Horizontal" should {
+		"include Left"   in { Left   must beLike { case x: Horizontal => ok } }
+		"include Center" in { Center must beLike { case x: Horizontal => ok } }
+		"include Right"  in { Right  must beLike { case x: Horizontal => ok } }
+	}
+
+	"Vertical" should {
+		"include Top"    in { Top    must beLike { case x: Vertical => ok } }
+		"include Center" in { Center must beLike { case x: Vertical => ok } }
+		"include Bottom" in { Bottom must beLike { case x: Vertical => ok } }
+	}
+
+	"Text" should {
+		"include Left"      in { Left      must beLike { case x: Text => ok } }
+		"include Center"    in { Center    must beLike { case x: Text => ok } }
+		"include Right"     in { Right     must beLike { case x: Text => ok } }
+		"include Justified" in { Justified must beLike { case x: Text => ok } }
+		"include Leading"   in { Leading   must beLike { case x: Text => ok } }
+		"include Trailing"  in { Trailing  must beLike { case x: Text => ok } }
+	}
+
+	// -----------------------------------------------------------------------
+	// opposite direction specification
+	// -----------------------------------------------------------------------
+
+	"Direction opposites" should {
+		"work for Top"         in { Top.         opposite must_== Bottom      }
+		"work for Bottom"      in { Bottom.      opposite must_== Top         }
+		"work for Left"        in { Left.        opposite must_== Right       }
+		"work for Right"       in { Right.       opposite must_== Left        }
+		"work for TopLeft"     in { TopLeft.     opposite must_== BottomRight }
+		"work for BottomRight" in { BottomRight. opposite must_== TopLeft     }
+		"work for TopRight"    in { TopRight.    opposite must_== BottomLeft  }
+		"work for BottomLeft"  in { BottomLeft.  opposite must_== TopRight    }
+	}
+}
