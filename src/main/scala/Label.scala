85b18bdb89875ac02ee031b51d00bbc989f14fb0
Christian Krause <kizkizzbangbang@googlemail.com>
2011-11-19 00:54:35 +0100

    Label as simplest Component

---
+++ src/main/scala/Label.scala
@@ -0,0 +1,37 @@
+/* **************************************************************************
+ *                                                                          *
+ *  Copyright (C)  2011  Christian Krause                                   *
+ *                                                                          *
+ *  Christian Krause <kizkizzbangbang@googlemail.com>                       *
+ *                                                                          *
+ ****************************************************************************
+ *                                                                          *
+ *  This file is part of 'scalawt'.                                         *
+ *                                                                          *
+ *  This project is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  any later version.                                                      *
+ *                                                                          *
+ *  This project is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this project. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+
+package scalawt
+
+/** A simple widget displaying text and/or an icon.
+  *
+  * Icons are optional, because, depending on the view, displaying images may
+  * not be supported (TUIs), i.e. one should always include a textual
+  * representation.
+  */
+class Label(text: String/*, icon: Option[Icon]*/) extends Component {
+  override def toString = "Label(%s)" format text
+}
